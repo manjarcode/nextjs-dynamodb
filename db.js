e986cb5172af95f801bc670bb4f36d38a2c12c1a
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2022-10-08 20:29:59 +0200

    removed linked local config file

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 import AWS from 'aws-sdk'
-import config from './config.secret.json'
 
 AWS.config.update({
-    accessKeyId: process.env.DB_ACCESS_KEY_ID ?? config.DB_ACCESS_KEY_ID,
-    secretAccessKey: process.env.DB_SECRET_ACCESS_KEY ?? config.DB_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.DB_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
     region: 'us-east-1'
   });
 
